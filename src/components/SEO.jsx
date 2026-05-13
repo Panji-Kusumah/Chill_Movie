351ee6ee2ab4c9cc49d76b33b4ad11858ba142fe
oryza0417-beep <oryza0417@gmail.com>
2026-05-13 10:30:04 +0700

    fix : SEO

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -7,18 +7,20 @@ const SEO = ({ title, description, image }) => {
             <meta name="description" content={description} />
             <meta name="robots" content="index, follow" />
             <link rel="canonical" href={window.location.href} />
-            <meta property="og:type" content="Website Hiburan & Tempat Nonton Film " />
+            <meta property="og:type" content="website" />
             <meta property="og:url" content={window.location.href} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
+            <meta property="og:title" content={`${title} | Chill Movie`} />
+            <meta property="og:description" content={description} /> 
             <meta property="og:image" content={image || defaultImage} />
+            <meta property="og:site_name" content="Chill Movie" />
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content={window.location.href} />
-            <meta property="twitter:title" content={title} />
-            <meta property="twitter:description" content={description} />
+            <meta property="twitter:title" content={`${title} | Chill Movie`} />
+            <meta property="twitter:description" content={description} /> 
             <meta property="twitter:image" content={image || defaultImage} />
-            <title>{title} | Chill</title>
+            <title>{title} | Chill Movie </title>
         </Helmet>
     );
 };
+
 export default SEO;
